88e124d017afb422c7afc13044b81fedc8366ae7
nibro7778 <mailme7778@gmail.com>
2020-05-07 02:07:16 +1000

    Added projects and folder structure

---
+++ MyDiary.Contacts.Web/Feature/Contacts/ContactsController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyDiary.Contacts.Web.Feature.Contacts
+{
+    public class ContactsController
+    {
+    }
+}
---
+++ MyDiary.Contacts.Web/Startup.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Nibro.Framework.StartupExtension;
+
+namespace MyDiary.Contacts.Web
+{
+    public class Startup
+    {
+
+        private static string ServiceName => typeof(Startup).Assembly.GetName().Name;
+        private readonly IWebHostEnvironment _env;
+        private readonly IConfiguration _configuration;
+
+        public Startup(IWebHostEnvironment env, ILoggerFactory logger)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            _configuration = builder.Build();
+            _env = env;
+
+            var log = logger.CreateLogger<Startup>();
+
+            log.LogInformation($"Starting Contacts service in  {env.EnvironmentName} environment");
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(options => { options.EnableEndpointRouting = false; })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+            services.EnableVersionedApi();
+            services.EnableHealthCheck(_configuration);
+            services.EnableSwagger(ServiceName);
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApiVersionDescriptionProvider provider)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+            else
+                app.UseHsts();
+
+            app.UseHttpsRedirection();
+            app.UseMvcWithDefaultRoute();
+
+            app.UseCustomHealthCheck();
+            app.UseCustomSwagger(provider);
+        }
+    }
+}
